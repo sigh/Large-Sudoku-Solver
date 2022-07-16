e07bf6ce9d87b06f0541d90ec4e7a880b459c0c2
sigh <sigh.projects@gmail.com>
2022-07-17 01:00:42 +1000

    Move output functions to new io::output module.

---
+++ src/io/output.rs
@@ -0,0 +1,40 @@
+use crate::{solver, types};
+
+pub fn solution_as_grid(constraint: &types::Constraint, solution: &solver::Solution) -> String {
+    let mut output = String::new();
+
+    let shape = &constraint.shape;
+    assert_eq!(shape.num_cells, solution.len());
+
+    let pad_size = shape.num_values.to_string().len() + 1;
+
+    for r in 0..shape.side_len {
+        for c in 0..shape.side_len {
+            let index = shape.make_cell_index(r, c);
+            let value = solution[index].to_string();
+            (0..pad_size - value.len()).for_each(|_| output.push(' '));
+            output.push_str(&value);
+        }
+        output.push('\n');
+    }
+
+    output
+}
+
+pub fn solution_compact(solution: &solver::Solution) -> String {
+    format!(
+        "[{}]",
+        solution
+            .iter()
+            .map(|v| v.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    )
+}
+
+pub fn counters(counters: &solver::Counters) -> String {
+    format!(
+        "{{ solutions: {} guesses: {} values_tried: {} progress_ratio: {} }}",
+        counters.solutions, counters.guesses, counters.values_tried, counters.progress_ratio
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,12 @@ use std::env;
 use std::fs;
 use std::rc::Rc;
 
-use large_sudoku_solver::io::parser;
+use large_sudoku_solver::io::{output, parser};
 use large_sudoku_solver::solver;
-use large_sudoku_solver::types;
 
 use indicatif::ProgressBar;
 use indicatif::ProgressStyle;
 use large_sudoku_solver::types::Constraint;
-
-fn grid_to_string(constraint: &types::Constraint, solution: &solver::Solution) -> String {
-    let mut output = String::new();
-
-    let shape = &constraint.shape;
-    assert_eq!(shape.num_cells, solution.len());
-
-    let pad_size = shape.num_values.to_string().len() + 1;
-
-    for r in 0..shape.side_len {
-        for c in 0..shape.side_len {
-            let index = shape.make_cell_index(r, c);
-            let value = solution[index].to_string();
-            (0..pad_size - value.len()).for_each(|_| output.push(' '));
-            output.push_str(&value);
-        }
-        output.push('\n');
-    }
-
-    output
-}
 
 fn run_solver(constraint: &Constraint) -> Vec<solver::Solution> {
     const SCALE: u64 = 10000;
@@ -42,19 +20,12 @@ fn run_solver(constraint: &Constraint) -> Vec<solver::Solution> {
     let closure_bar = bar.clone();
     let progress_callback = Box::new(move |counters: &solver::Counters| {
         closure_bar.set_position((counters.progress_ratio * (SCALE as f64)) as u64);
-        closure_bar.set_message(format!("{:?}", counters));
+        closure_bar.set_message(output::counters(counters));
     });
 
     let mut solutions = Vec::new();
     for solution in solver::solution_iter(constraint, Some(progress_callback)).take(2) {
-        bar.println(format!(
-            "[{}]",
-            solution
-                .iter()
-                .map(|v| v.to_string())
-                .collect::<Vec<_>>()
-                .join(" ")
-        ));
+        bar.println(output::solution_compact(&solution));
         solutions.push(solution);
     }
 
@@ -78,6 +49,6 @@ fn main() {
     let constraint = parser::parse_text(&input).expect("Could not parse input file.");
 
     for solution in run_solver(&constraint) {
-        println!("{}", grid_to_string(&constraint, &solution));
+        println!("{}", output::solution_as_grid(&constraint, &solution));
     }
 }
